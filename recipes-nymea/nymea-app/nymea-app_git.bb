nymea/meta-nymea

commit 3db5f850dd909a70115061b5fd7b1ec55a3f41ff

Update License to GPLv3 and LGPv3 or later for all recipes

recipes-nymea/nymea-app/nymea-app_git.bb
@@ -3,8 +3,11 @@ SUMMARY = "Client application for the nymea daemon"
 HOMEPAGE = "https://nymea.io"
 BUGTRACKER = "https://github.com/nymea/nymea-app/issues"
 
-LICENSE = "GPL-3.0-only | NYMEA_COMMERCIAL"
-LIC_FILES_CHKSUM="file://LICENSE;md5=1ebbd3e34237af26da5dc08a4e440464"
+LICENSE = "(GPL-3.0-or-later & LGPL-3.0-or-later)"
+LIC_FILES_CHKSUM = " \
+	file://LICENSE.GPL3;md5=1ebbd3e34237af26da5dc08a4e440464 \
+	file://LICENSE.LGPL3;md5=3000208d539ec061b899bce1d9ce9404 \
+	"
 
 SRC_URI="git://github.com/nymea/nymea-app.git;protocol=https;branch=master"
 

conf/layer.conf
@@ -15,5 +15,3 @@ BBFILE_PRIORITY_nymea = "10"
 LAYERDEPENDS_nymea += "openembedded-layer"
 
 LAYERSERIES_COMPAT_nymea = "kirkstone"
-
-LICENSE_PATH += "${LAYERDIR}/licenses"

recipes-nymea/libnymea-networkmanager/libnymea-networkmanager_git.bb
@@ -3,8 +3,11 @@ SUMMARY = "Library for nymea in order to communicate with the network-manager us
 HOMEPAGE = "https://nymea.io"
 BUGTRACKER = "https://github.com/nymea/libnymea-networkmanager/issues"
 
-LICENSE = "LGPL-3.0-only | NYMEA-COMMERCIAL"
-LIC_FILES_CHKSUM="file://LICENSE.LGPL3;md5=3000208d539ec061b899bce1d9ce9404"
+LICENSE = "(LGPL-3.0-or-later)"
+LIC_FILES_CHKSUM = " \
+	file://LICENSE.GPL3;md5=1ebbd3e34237af26da5dc08a4e440464 \
+	file://LICENSE.LGPL3;md5=3000208d539ec061b899bce1d9ce9404 \
+	"
 
 SRC_URI="git://github.com/nymea/libnymea-networkmanager.git;protocol=https;branch=master"
 # Release: 1.12.3

recipes-nymea/nymea-experience-plugin-energy/nymea-experience-plugin-energy_git.bb
@@ -3,7 +3,7 @@ SUMMARY = "Zeroconf plugin for nymea in order to interact with the avahi-daemon"
 HOMEPAGE = "https://nymea.io"
 BUGTRACKER = "https://github.com/nymea/nymea-zeroconf-plugin-avahi/issues"
 
-LICENSE = "GPL-3.0-or-later | NYMEA-COMMERCIAL"
+LICENSE = "GPL-3.0-or-later"
 LIC_FILES_CHKSUM = "file://LICENSE.GPL3;md5=1ebbd3e34237af26da5dc08a4e440464"
 
 SRC_URI = "git://github.com/nymea/nymea-experience-plugin-energy.git;protocol=https;branch=master"

recipes-nymea/nymea-gpio/nymea-gpio_git.bb
@@ -3,10 +3,13 @@ SUMMARY = "Library and utils to interact with GPIOs for nymea"
 HOMEPAGE = "https://nymea.io"
 BUGTRACKER = "https://github.com/nymea/nymea-gpio/issues"
 
-LICENSE = "(LGPL-3.0-only & GPL-3.0-only) | NYMEA-COMMERCIAL"
-LICENSE:${PN}-utils = "GPL-3.0-only | NYMEA-COMMERCIAL"
+LICENSE = "(GPL-3.0-or-later & LGPL-3.0-or-later)"
 
-LIC_FILES_CHKSUM=" \
+LICENSE:${PN}-utils = "GPL-3.0-or-later"
+LICENSE:lib${PN} = "LGPL-3.0-or-later"
+LICENSE:${PN}-dev = "LGPL-3.0-or-later"
+
+LIC_FILES_CHKSUM = " \
 	file://LICENSE.LGPL3;md5=3000208d539ec061b899bce1d9ce9404 \
 	file://LICENSE.GPL3;md5=1ebbd3e34237af26da5dc08a4e440464 \
 	"

recipes-nymea/nymea-mqtt/nymea-mqtt_git.bb
@@ -3,7 +3,13 @@ SUMMARY = "Library and utils for native MQTT support in nymea"
 HOMEPAGE = "https://nymea.io"
 BUGTRACKER = "https://github.com/nymea/nymea-mqtt/issues"
 
-LICENSE = "(GPL-3.0-only & LGPL-3.0-only) | NYMEA-COMMERCIAL"
+LICENSE = "(GPL-3.0-or-later & LGPL-3.0-or-later)"
+
+LICENSE:${PN}-client = "GPL-3.0-or-later"
+LICENSE:${PN}-server = "GPL-3.0-or-later"
+LICENSE:lib${PN} = "LGPL-3.0-or-later"
+LICENSE:lib${PN}-dev = "LGPL-3.0-or-later"
+
 LIC_FILES_CHKSUM = " \
 	file://LICENSE.LGPL3;md5=3000208d539ec061b899bce1d9ce9404 \
 	file://LICENSE.GPL3;md5=1ebbd3e34237af26da5dc08a4e440464 \

recipes-nymea/nymea-plugins-genericthings/nymea-plugins-genericthings_git.bb
@@ -3,8 +3,8 @@ SUMMARY = "Collection of generic integration plugins for the nymea daemon"
 HOMEPAGE = "https://nymea.io"
 BUGTRACKER = "https://github.com/nymea/nymea-plugins-genericthings/issues"
 
-LICENSE = "LGPL-3.0-only | NYMEA-COMMERCIAL"
-LIC_FILES_CHKSUM = "file://LICENSE.LGPL3;md5=3000208d539ec061b899bce1d9ce9404"
+LICENSE = "GPL-3.0-or-later"
+LIC_FILES_CHKSUM = "file://LICENSE.GPL3;md5=1ebbd3e34237af26da5dc08a4e440464"
 
 SRC_URI = "git://github.com/nymea/nymea-plugins-genericthings.git;protocol=https;branch=master"
 # Release: 1.12.3

commit 95bbf994f207bf3df534f2ba937d06d0db7ca10b

nymea-app: update LGPL3 license file checksum
This fixes:

  ERROR: nymea-app-1.10.13-gitAUTOINC+b26d959ca5-r0 do_populate_lic: ↵
    QA Issue: nymea-app: The LIC_FILES_CHKSUM does not match for ↵
    file://LICENSE.LGPL3

Fixes: 3db5f85 ("Update License to GPLv3 and LGPv3 or later for all recipes")
Signed-off-by: Michael Heimpold <mhei@heimpold.de>
Reviewed-by: Quentin Schulz <quentin.schulz@cherry.de>

recipes-nymea/nymea-app/nymea-app_git.bb
@@ -6,7 +6,7 @@ BUGTRACKER = "https://github.com/nymea/nymea-app/issues"
 LICENSE = "(GPL-3.0-or-later & LGPL-3.0-or-later)"
 LIC_FILES_CHKSUM = " \
 	file://LICENSE.GPL3;md5=1ebbd3e34237af26da5dc08a4e440464 \
-	file://LICENSE.LGPL3;md5=3000208d539ec061b899bce1d9ce9404 \
+	file://LICENSE.LGPL3;md5=8a5c0c154112c14f0d9713ac9e981ee6 \
 	"
 
 SRC_URI="git://github.com/nymea/nymea-app.git;protocol=https;branch=master"
@@ -26,4 +26,3 @@ do_install:append() {
 	# FIXME: drop icons, unneeded on this platform
 	rm -rf ${D}/usr/share/icons/
 }
-